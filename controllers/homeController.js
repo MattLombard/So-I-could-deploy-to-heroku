5d068f61412503073d6f7a785b1026156ea7ec92
Matt Lombard <ncmattlombard@gmail.com>
2023-04-21 19:32:11 -0400

    uncommented a line of code

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,9 +7,10 @@ router.get('/', async (req, res) => {
     const albumData = await Album.findAll({});
 
     const albums = albumData.map((album) => album.get({ plain: true }));
+
     res.render('dashboard', { albums, logged_in: req.session.logged_in });
 
-    // res.status(200).json(albums);
+    res.status(200).json(albums);
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving albums', error });
   }
